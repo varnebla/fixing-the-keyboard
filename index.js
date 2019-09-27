7e034489c24dda84cd91c974e7f37326fe2cc4d7
Victor Arnedo Blanco <victor@Air-de-Victor.lan>
2019-09-27 19:45:50 +0200

    done

--- index.js
+++ index.js
@@ -13,8 +13,20 @@
 
 findBrokenKeys = (str1, str2) => {
   // write your code HERE
-
-
+  const arr1 = str1.split('');
+  const arr2 = str2.split('');
+  if(str1 === str2) return -1
+  const result = [];
+  for (let i = 0; i<arr1.length ; i++) {
+    if (arr1[i] !== arr2[i]){
+      
+      if(result.indexOf(arr1[i]) === -1) result.push(arr1[i]);
+      
+    }
+    
+    console.log(result);
+  }
+return result;
 
 };
 
